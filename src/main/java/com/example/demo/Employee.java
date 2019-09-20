d1a3e2bb94003f5c44c0aadd4852bb4759210992
amit <amit@amit-PC>
2019-09-20 05:15:42 -0700

    Latest Completed project

---
+++ src/main/java/com/example/demo/Employee.java
@@ -0,0 +1,58 @@
+package com.example.demo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+	@Id@GeneratedValue(strategy=GenerationType.AUTO)
+	private int id;
+	@Column(name="name")
+	private String name;
+	@Column(name="email")
+	private String email;
+	@Column(name="salary")
+	private Double salary;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Double getSalary() {
+		return salary;
+	}
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+	public Employee(int id, String name, String email, Double salary) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.salary = salary;
+	}
+	public Employee() {
+		super();
+	}
+	
+ 
+	
+	
+
+}
---
+++ src/main/java/com/example/demo/MyJpaRepo.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface MyJpaRepo extends JpaRepository<Employee,Integer> {
+	
+	@Query("from Employee")
+	List<Employee> getEmployee();
+	
+	@Query("from Employee where salary>:salary")
+	List<Employee> getSalaryGreater(@Param("salary") double salary);
+	
+	@Query("from Employee where salary<:salary")
+	List<Employee> getSalaryLess(@Param("salary") double salary);
+
+}
